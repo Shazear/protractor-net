e69e15d467528563a48a5e5a64a504564c730be2
Steve Trotman <steve.trotman@renaissance.com>
2017-07-05 14:23:53 -0500

    build debug mode

--- automation/docker/buildInContainer.ps1
+++ automation/docker/buildInContainer.ps1
@@ -3,5 +3,5 @@ w:
 nuget restore -Verbosity detailed
 Set-Location .\src\Protractor
 #set version number in assemblyinfo and nuspec
-msbuild .\Protractor-NET40.csproj /target:Build /p:Configuration=Release /p:Platform=AnyCPU
+msbuild .\Protractor-NET40.csproj /target:Build /p:Configuration=Debug /p:Platform=AnyCPU
 nuget pack
