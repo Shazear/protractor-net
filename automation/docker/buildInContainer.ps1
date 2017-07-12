7fb0244df3f2e5c421c08123fb5faedb1d143962
Steve Trotman <steve.trotman@renaissance.com>
2017-07-12 13:12:04 -0500

    fix command ordering

--- automation/docker/buildInContainer.ps1
+++ automation/docker/buildInContainer.ps1
@@ -1,8 +1,8 @@
 subst w: c:\workspace
 w:
-nuget restore -Verbosity detailed
-Set-Location .\src\Protractor
 Write-Host("Version passed in: $($args[0])")
 .\AssemblyInfoPatchVersion.ps1 $($args[0])
+nuget restore -Verbosity detailed
+Set-Location .\src\Protractor
 msbuild .\Protractor-NET40.csproj /target:Build /p:Configuration=Debug /p:Platform=AnyCPU
 nuget pack -Version $($args[0])
